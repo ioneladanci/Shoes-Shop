0363e576b2f02562a48e1bca409c4474d3cde329
ioneladanci <ioneladanci7@gmail.com>
2023-05-19 10:43:15 +0300

    Proiect final

---
+++ demo/src/main/java/com/example/demo/config/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.ws.rs.HttpMethod;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests((authz) -> authz.anyRequest().permitAll());
+        return http.build();
+    }
+
+}
